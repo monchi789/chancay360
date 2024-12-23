7a88e63d7c7c058cf101c735aae78c7b9e9018d7
h3nr1J <d.henry.7569@gmail.com>
2024-12-22 22:40:52 -0500

    Añadiendo Main del Componente Publication sin modela

--- dashboard/src/modules/publication/pages/PublicationMain.tsx
+++ dashboard/src/modules/publication/pages/PublicationMain.tsx
@@ -1,56 +1,20 @@
 import Title from "@/shared/common/Title.tsx";
 import PublicationListCard from "@/modules/publication/components/PublicationListCard";
-import { useState } from "react";
 
-import PublicationCardCreate from "@/modules/publication/components/PublicationCardCreate.tsx";
-
-const PublicationMain = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  // Función para abrir el modal
-  const openModal = () => setShowModal(true);
-
-  // Función para cerrar el modal
-  const closeModal = () => setShowModal(false);
-
+const PublicationMain: React.FC = () => {
   return (
-    <>
-      {/* Encabezado con botón para abrir el modal */}
-      <div className="p-4 sm:p-6 md:p-8">
-        <Title
-          title="Publicacion"
-          description="Aquí puedes gestionar las publicaciones"
-          buttonName="Crear Publicación"
-          openModal={openModal} // Función que abre el modal
-        />
-      </div>
+    <div className="space-y-6">
+      {/* Encabezado con título, descripción y botón para redirigir */}
+      <Title
+        title="Publicaciones"
+        description="Aquí puedes gestionar las publicaciones"
+        buttonName="Crear Publicación"
+        link="/publicacion/crear"
+      />
 
-      {/* Listado de publicaciones */}
-      <div className="p-4 sm:p-6 md:p-8">
-        <PublicationListCard />
-      </div>
-
-      {/* Modal para crear una nueva publicación */}
-      {showModal && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50 p-4">
-          <div
-            className="bg-white p-6 sm:p-8 rounded-lg shadow-lg w-full 
-                       max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl relative"
-          >
-            {/* Botón para cerrar el modal */}
-            <button
-              onClick={closeModal}
-              className="absolute top-3 right-3 text-gray-400 hover:text-gray-600 transition-all duration-300"
-            >
-              &times;
-            </button>
-
-            {/* Componente de creación de publicación */}
-            <PublicationCardCreate onClose={closeModal} />
-          </div>
-        </div>
-      )}
-    </>
+      {/* Lista de publicaciones */}
+      <PublicationListCard />
+    </div>
   );
 };
 
