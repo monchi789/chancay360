fbe316e2877d10a745c9532e9cd65f0d71be972c
Cristian Monzon <cristian-02-@live.com>
2025-01-29 10:57:21 -0500

    Agregando

--- dashboard/wait_for_backend.sh
+++ dashboard/wait_for_backend.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
-
-# Animate the dance
-dance() {
-  frames=(
-    "💃   (o_o)  💃\n    \_/\     /\_/"
-    "💃  (o_o)   💃\n     /\_/     \_/\"
-    "💃 (o_o)    💃\n    \_/\     /\_/"
-    "💃  (o_o)   💃\n     /\_/     \_/\"
-    "💃   (o_o)  💃\n    \_/\     /\_/"
-  )
-  for frame in "${frames[@]}"; do
-    echo -e "$frame"
-    sleep 0.2
-    clear
-  done
-}
-
-# Wait for the backend to be available
-while ! nc -z -v -w30 db 3000; do
-  dance
+# Espera a que la base de datos esté disponible
+until nc -z -v -w30 db 3000
+do
+  echo "Esperando a que el backend este disponible..."
+  sleep 1
 done
+
+echo "Backend listo, ejecutando frontend..."
